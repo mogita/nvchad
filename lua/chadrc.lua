cdbc0dc789630b1ef08145aedb59024fc4ac7e57
mogita <me@mogita.com>
2025-09-24 23:00:41 +0800

    feat: turn off transparency

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-  theme = "tokyodark",
-  transparency = true,
+  theme = "tokyonight",
+  -- transparency = true,
 
   -- hl_override = {
   -- 	Comment = { italic = true },
