7153d3580de74f77ef5b49909b8e8a63366f98aa
mogita <me@mogita.com>
2025-09-24 20:27:41 +0800

    feat: migrate key mappings

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+-- full list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local servers = { "html", "cssls", "ts_ls", "gopls", "golangci_lint_ls", "tailwindcss", "rust_analyzer", "jsonls", "biome" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,38 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- Disable default NvChad mappings that conflict with custom ones
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-l>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-s>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Custom mappings ported from nvchad1
+
+-- Split panes
+map("n", "ss", ":split<Return><C-w>w", { desc = "Split horizontal pane" })
+map("n", "sv", ":vsplit<Return><C-w>w", { desc = "Split vertical pane" })
+
+-- Switch between windows (using Shift+Alt combinations)
+map("n", "<S-M-j>", "<C-w>h", { desc = "Window left" })
+map("n", "<S-M-l>", "<C-w>l", { desc = "Window right" })
+map("n", "<S-M-k>", "<C-w>j", { desc = "Window down" })
+map("n", "<S-M-i>", "<C-w>k", { desc = "Window up" })
+
+-- Resize windows
+map("n", "<C-w><left>", "<C-w><", { desc = "Resize window to left" })
+map("n", "<C-w><right>", "<C-w>>", { desc = "Resize window to right" })
+map("n", "<C-w><up>", "<C-w>+", { desc = "Resize window upward" })
+map("n", "<C-w><down>", "<C-w>-", { desc = "Resize window downward" })
+
+-- Save file with comma+w
+map("n", ",w", "<cmd> w <CR>", { desc = "Save file" })
+
+-- Change without yanking
+map("n", "c", '"_c', { desc = "Change without yanking" })
+map("n", "C", '"_C', { desc = "Change till eol without yanking" })
+
+-- Ctrl+Enter to insert new line
+map("n", "<C-cr>", "<End><cr>i", { desc = "Insert new line from current cursor position" })
+map("i", "<C-cr>", "<End><cr>", { desc = "Insert new line from current cursor position" })
