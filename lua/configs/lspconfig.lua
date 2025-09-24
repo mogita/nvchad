25220a81eae61df06bcfc727518ca18a33fa1114
mogita <me@mogita.com>
2025-09-24 22:31:51 +0800

    feat: sync custom plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- full list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-local servers = { "html", "cssls", "ts_ls", "gopls", "golangci_lint_ls", "tailwindcss", "rust_analyzer", "jsonls", "biome" }
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "gopls",
+  "golangci_lint_ls",
+  "tailwindcss",
+  "rust_analyzer",
+  "jsonls",
+  "biome",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,43 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- always turn on blame on the current line
+    },
+  },
+  { "wakatime/vim-wakatime", lazy = false },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "smoka7/hop.nvim",
+    lazy = false,
+    version = "*",
+    config = function()
+      require("hop").setup {
+        keys = "etovxqpdygfblzhckisuran",
+      }
+      vim.api.nvim_set_keymap("n", "f", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  { "akinsho/git-conflict.nvim", version = "*", config = true },
+  { "augmentcode/augment.vim", lazy = false },
 }
